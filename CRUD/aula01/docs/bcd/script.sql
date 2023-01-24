42e49f545198aee77097140b7b91f3ea8296f780
batista29 <natabatista2908@gmail.com>
2023-01-24 10:29:12 -0300

    Banco atualizado

--- CRUD/aula01/docs/bcd/script.sql
+++ CRUD/aula01/docs/bcd/script.sql
@@ -23,22 +23,41 @@ create table pedidos(
     foreign key (id_entregador) references entregadores(id_entregador) on delete cascade
 );
 
-LOAD DATA INFILE 'C:/Users/SUPORTE/Desktop/senai/3DES/CRUD/aula01/docs/csv/entregadores.csv'
+LOAD DATA INFILE 'C:/Users/SUPORTE/Desktop/entregas/CRUD/aula01/docs/csv/entregadores.csv'
 INTO TABLE entregadores
 FIELDS TERMINATED BY ';'
 ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n'
 IGNORE 1 ROWS;
 
-LOAD DATA INFILE 'C:/Users/SUPORTE/Desktop/senai/3DES/CRUD/aula01/docs/csv/pedidos.csv'
+LOAD DATA INFILE 'C:/Users/SUPORTE/Desktop/entregas/CRUD/aula01/docs/csv/pedidosNew.csv'
 INTO TABLE pedidos
 FIELDS TERMINATED BY ';'
 ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n'
 IGNORE 1 ROWS;
 
-drop view if exists vw_gestao_entregas;
-create view vw_gestao_entregas AS 
-SELECT e.id_entregador, e.nome, p.hora_pedido, p.hora_entrega, p.hora_fim
+drop view if exists vw_entregador;
+create view vw_entregador AS 
+SELECT e.nome,p.id_pedido,p.cliente,p.produto,p.endereco,p.data, p.hora_pedido, p.hora_entrega
 FROM entregadores e INNER JOIN pedidos p on e.id_entregador = p.id_entregador;
-select * from vw_gestao_entregas;
+
+drop view if exists vw_cozinha;
+create view vw_cozinha AS 
+SELECT p.id_pedido,p.cliente,p.produto,p.endereco,p.data, p.hora_pedido
+FROM entregadores e INNER JOIN pedidos p on e.id_entregador = p.id_entregador;
+
+
+-- Listar pedidos em execução, com os campos hora_entrega e hora_fim em branco ou nulo ou hora 0:00 PEDIDOS EM EXECUCAO
+drop view if exists vw_execucao;
+create view vw_execucao AS 
+SELECT e.nome,p.id_pedido,p.cliente,p.endereco,p.produto,p.data, p.hora_pedido, p.hora_entrega, p.hora_fim
+FROM entregadores e INNER JOIN pedidos p
+on e.id_entregador = p.id_entregador
+WHERE p.hora_entrega = "00:00:00" or p.hora_entrega = null;
+
+-- PROCURAR PELO NOME DO ENTREGADOR
+drop view if exists vw_pedidos_entregador;
+create view vw_pedidos_entregador AS 
+SELECT e.id_entregador, e.nome, p.hora_pedido, p.hora_entrega, p.hora_fim
+FROM entregadores e INNER JOIN pedidos p WHERE e.nome = "Jair" AND p.hora_fim = "00:00:00";
